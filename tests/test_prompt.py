f4dc627bdadc4aa7b6b75df28057c9812e6f74e6
Francis Rutledge <fresh@Franciss-MacBook-Pro-2.local>
2025-04-26 12:31:32 -0400

    Fix E702: split multiple statements into separate lines

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -1,5 +1,10 @@
 from main import build_prompt
-class Dummy: pass
+class Dummy:
+    pass
 def test_event():
- d=Dummy(); d.event='Gala'; d.date='2025-05-30'; d.tone='upbeat'
- assert 'Gala' in build_prompt(d)
+    d = Dummy()
+    d.event = 'Gala'
+    d.date = '2025-05-30'
+    d.time = '12:00'
+    d.tone = 'upbeat'
+    assert 'Gala' in build_prompt(d)
